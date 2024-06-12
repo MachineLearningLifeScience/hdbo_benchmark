db2c4d01df7ddf23c3712faed9f88505c757af35
Miguel González Duque <miguelgondu@gmail.com>
2024-06-12 14:02:21 +0200

    Adds new tables and table-processing scripts

---
+++ create_table.py
@@ -0,0 +1,114 @@
+"""
+This script creates a table in pandas, downloading the data
+from wandb.
+
+The model for each row of the base table goes like this:
+{
+    "model": "model_name",
+    "function": "function_name",
+    "step": "step_value",
+    "x": "x_value",
+    "y": "y_value",
+    "best_y": "best_y_value_so_far",
+}
+
+After that, we can create a summary table with the best_y
+of each model for each function. The structure of this table
+goes like this:
+
+{
+    "function": "function_name",
+    "model_1": "best_y_value",
+    "model_2": "best_y_value",
+    ...
+    "model_n": "best_y_value",
+}
+"""
+
+from pathlib import Path
+import pandas as pd
+
+import wandb
+
+from download_from_wandb import get_all_runs_for_experiment
+
+ROOT_DIR = Path(__file__).parent.resolve()
+
+
+def convert_data_to_dataframes(
+    all_data: list[wandb.apis.public.Run],
+) -> list[pd.DataFrame]:
+    dfs: list[pd.DataFrame] = []
+    for i, run in enumerate(all_data):
+        print(f"Processing run {i + 1}/{len(all_data)}")
+        df = run.history()
+        df["solver_name"] = run.config["solver_name"]
+        df["function_name"] = run.config["function_name"]
+        df["seed"] = run.config["seed"]
+        df["poli_hash"] = run.config["poli_hash"]
+        df["hdbo_benchmark_hash"] = run.config["hdbo_benchmark_hash"]
+        df["poli_baselines_hash"] = run.config["poli_baselines_hash"]
+        df["state"] = run.state
+        dfs.append(df)
+
+    return dfs
+
+
+def create_base_table(
+    n_dimensions: int = 128,
+    save_cache: bool = True,
+    use_cache: bool = False,
+    tags: list[str] | None = None,
+) -> pd.DataFrame:
+    CACHE_PATH = ROOT_DIR / "data" / "results_cache"
+    CACHE_PATH.mkdir(exist_ok=True, parents=True)
+    tags_str = "-".join(tags) if tags is not None else "all"
+    CACHE_FILE = (
+        CACHE_PATH / f"base_table-n_dimensions-{n_dimensions}-tags-{tags_str}.csv"
+    )
+
+    if use_cache and CACHE_FILE.exists():
+        df = pd.read_csv(CACHE_FILE)
+        return df
+
+    all_runs = get_all_runs_for_experiment(
+        experiment_name="benchmark_on_pmo", n_dimensions=n_dimensions, tags=tags
+    )
+
+    # Append with the results from PR on 2D
+    if n_dimensions != 2:
+        pr_runs = get_all_runs_for_experiment(
+            experiment_name="benchmark_on_pmo",
+            solver_name="pr",
+            n_dimensions=2,
+            tags=tags,
+        )
+        all_runs.extend(pr_runs)
+
+    # Append the results of Bounce on 128D
+    if n_dimensions != 128:
+        bounce_runs = get_all_runs_for_experiment(
+            experiment_name="benchmark_on_pmo",
+            solver_name="bounce",
+            n_dimensions=128,
+            tags=tags,
+        )
+        all_runs.extend(bounce_runs)
+
+    all_dfs = convert_data_to_dataframes(all_runs)
+
+    df = pd.concat(all_dfs)
+    if save_cache:
+        df.to_csv(CACHE_FILE, index=False)
+
+    return df
+
+
+if __name__ == "__main__":
+    df = create_base_table(
+        n_dimensions=2,
+        save_cache=True,
+        use_cache=False,
+        tags=None,
+    )
+    print(df)
---
+++ download_from_wandb.py
@@ -0,0 +1,43 @@
+import wandb
+
+WANDB_PROJECT = "hdbo-embeddings-benchmark"
+WANDB_ENTITY = "hdbo-benchmark"
+
+
+def get_all_runs_for_experiment(
+    experiment_name: str,
+    solver_name: str | None = None,
+    function_name: str | None = None,
+    n_dimensions: int | None = None,
+    seed: int | None = None,
+    tags: list[str] | None = None,
+) -> list[wandb.apis.public.Run]:
+    api = wandb.Api()
+
+    filter_: dict[str, str | int | dict] = {
+        "state": {"$in": ["finished", "running", "failed", "crashed"]},
+    }
+    if solver_name is not None:
+        filter_["config.solver_name"] = solver_name
+
+    if function_name is not None:
+        filter_["config.function_name"] = function_name
+
+    if n_dimensions is not None:
+        # If it's a discrete solver, we don't need to filter by n_dimensions
+        if solver_name in ["bounce", "pr"]:
+            pass
+        else:
+            filter_["config.n_dimensions"] = n_dimensions
+
+    if seed is not None:
+        filter_["config.seed"] = seed
+
+    if tags is not None:
+        filter_["tags"] = {"$in": tags}
+
+    runs = api.runs(
+        f"{WANDB_ENTITY}/{experiment_name}",
+        filter_ if filter_ else None,
+    )
+    return list(runs)
