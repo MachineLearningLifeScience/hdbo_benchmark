9fda406e0a1a754ed3b1fdb93cba1905b7982201
Miguel González Duque <miguelgondu@gmail.com>
2024-10-14 17:54:39 +0200

    removes conda env creation from batch

--- batch.sh
+++ batch.sh
@@ -17,36 +17,6 @@ MAX_ITER=$4
 TAG=$5
 ENV_NAME=$6
 
-# Assuming that conda is already activated...
-# Creating the conda env if it doesn't exist
-if [ ! -d "$CONDA_PREFIX/envs/${ENV_NAME}" ]; then
-    conda create -n ${ENV_NAME} python=3.10 -y
-fi
-
-# Making sure the dependencies are there
-if [ ${ENV_NAME} == "hdbo_ax" ]; then
-    conda run -n ${ENV_NAME} pip install -e ".[ax]"
-fi
-if [ ${ENV_NAME} == "hdbo_bounce" ]; then
-    conda run -n ${ENV_NAME} pip install -e ".[bounce]"
-fi
-if [ ${ENV_NAME} == "hdbo_baxus" ]; then
-    conda run -n ${ENV_NAME} pip install -e ".[baxus]"
-fi
-if [ ${ENV_NAME} == "hdbo_lambo2" ]; then
-    conda run -n ${ENV_NAME} pip install -e ".[lambo2]"
-fi
-if [ ${ENV_NAME} == "hdbo_pr" ]; then
-    conda run -n ${ENV_NAME} pip install -e ".[pr]"
-fi
-if [ ${ENV_NAME} == "hdbo_alebo" ]; then
-    conda run -n ${ENV_NAME} pip install -e ".[alebo]"
-fi
-if [ ${ENV_NAME} == "hdbo_benchmark" ]; then
-    conda run -n ${ENV_NAME} pip install -e "."
-fi
-
-
 # Add your commands here
 command="conda run -n ${ENV_NAME} python run.py \
         --function-name=${FUNCTION_NAME} \
