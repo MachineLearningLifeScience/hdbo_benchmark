b49823a531494e313b1696a5837e52493a20a777
Miguel González Duque <miguelgondu@gmail.com>
2024-11-05 13:26:48 +0100

    Moves some assets to inside the package

--- src/hdbo_benchmark/utils/experiments/load_metadata_for_vaes.py
+++ src/hdbo_benchmark/utils/experiments/load_metadata_for_vaes.py
@@ -1,20 +1,21 @@
+from pathlib import Path
+
 import json
 
-from hdbo_benchmark.utils.constants import ROOT_DIR
+from hdbo_benchmark.assets import __file__ as assets_file
+
+ASSETS_DIR = Path(assets_file).parent
 
 
 def load_alphabet_for_pmo() -> dict[str, int]:
-    METADATA_PATH = ROOT_DIR / "data" / "small_molecule_datasets" / "processed"
-
-    with open(METADATA_PATH / "zinc250k_alphabet_stoi.json") as fp_alphabet:
+    with open(ASSETS_DIR / "zinc250k_alphabet_stoi.json") as fp_alphabet:
         alphabet: dict[str, int] = json.load(fp_alphabet)
 
     return alphabet
 
 
 def load_sequence_length_for_pmo() -> int:
-    METADATA_PATH = ROOT_DIR / "data" / "small_molecule_datasets" / "processed"
-    with open(METADATA_PATH / "zinc250k_metadata.json") as fp_metadata:
+    with open(ASSETS_DIR / "zinc250k_metadata.json") as fp_metadata:
         metadata: dict[str, int] = json.load(fp_metadata)
 
     return metadata["max_sequence_length"]
